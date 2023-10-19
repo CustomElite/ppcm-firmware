48fbfb258774893a173af32f4c6a924f0b097e82
CustomElite <mark.webcraft@gmail.com>
2023-10-19 03:27:26 -0400

    wip

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "peripherals.h"
+#include "etl/vector.h"
 
 #include "stm32f1xx_ll_rcc.h"
 #include "stm32f1xx_ll_bus.h"
@@ -7,6 +8,7 @@
 #include "stm32f1xx_ll_utils.h"
 #include "stm32f1xx_ll_system.h"
 #include "stm32f1xx_ll_cortex.h"
+#include <stddef.h>
 
 int main()
 {
@@ -14,13 +16,21 @@ int main()
     LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
     NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
+    etl::vector<size_t, 8> vec{200, 500, 750, 1000, 1500, 2000, 3000, 5000};
+
     SysClock_Init();
     GPIO_Init();
 
     while (1) 
     {
-        LL_GPIO_TogglePin(STATUS_LED_PORT, STATUS_LED_PIN);
-        LL_mDelay(5000u);
+        for (auto& v : vec)
+        {
+            for (size_t i = 0; i < 6; ++i)
+            {
+                LL_GPIO_TogglePin(STATUS_LED_PORT, STATUS_LED_PIN);
+                LL_mDelay(v);
+            }
+        }
     }
     
     return 0;
